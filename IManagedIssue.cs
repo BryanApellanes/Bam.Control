752f24ba5f007af6942f0acf15f10099cd388058
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-09-04 06:18:59 -0700

    wip: issue tracking

---
+++ IManagedIssue.cs
@@ -0,0 +1,9 @@
+﻿namespace Bam.Control
+{
+    public interface IManagedIssue
+    {
+        string Id { get; set; }
+        string Title { get; set; }
+        string Body { get; set; }
+    }
+}
