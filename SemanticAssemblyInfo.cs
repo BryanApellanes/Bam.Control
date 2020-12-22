d9fd822719a749684be2a98255600a0c24668de9
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-22 10:03:59 -0800

    Reconfigured with latest build scripts

---
+++ SemanticAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyCommit("9e58023")]
+[assembly: AssemblySemanticVersion("1.0.5-test+9e58023")]
+[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+9e58023, Revision=1.0.5.2580904422")]
+[assembly: AssemblyProduct("bam.control")]
