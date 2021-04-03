69ca3c0a862b70674b7b9b1e9f1fb14dbdf5eeba
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-04-02 18:31:43 -0700

    [wip] issue tracking

--- IManagedIssue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bam.Control
-{
-    public interface IManagedIssue
-    {
-        string Id { get; set; }
-        string Title { get; set; }
-        string Body { get; set; }
-    }
-}
